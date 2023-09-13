0534c638ed9ace38b1268a0b006f418c6fe00975
unknown <2482173874@qq.com>
2023-09-13 22:11:04 +0800

    feature-lzy

--- pdf.py
+++ pdf.py
@@ -1,11 +1,17 @@
 import os
 import re
+import requests
 
 def generate_text(prompt):
-    """
-    TODO
-    """
-    pass
+    result = requests.post(
+        'http://localhost:8080/v1/chat/completions',
+        json = {
+            'model': 'gpt-3.5-turbo',
+            'prompt': prompt,
+            'temperature': 0.7,
+        }
+    )
+    return result
 
 def generate_answer(current_file_text: str, content: str):
     """
@@ -14,10 +20,16 @@ def generate_answer(current_file_text: str, content: str):
     pass
 
 def generate_summary(current_file_text: str):
-    """
-    TODO
-    """
-    pass
+    content = f"Act as a summarizer. Please summarize the following content:\n\n{current_file_text}"
+    summary_prompt = requests.post(
+        'http://localhost:8080/v1/chat/completions',
+        json = {
+            'model': 'gpt-3.5-turbo',
+            'message': content,
+            'temperature': 0.7
+        }
+    )
+    return summary_prompt
 
 if __name__ == "__main__":
     prompt = generate_answer("Hello", "Who is Sun Wukong?")
