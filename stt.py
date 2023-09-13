b6ca937b9274f8e8b7002f389b964890743ff18b
Sherry-1023 <126405492+Sherry-1023@users.noreply.github.com>
2023-09-13 20:50:20 +0800

    完成语音输入

--- stt.py
+++ stt.py
@@ -1,35 +1,26 @@
 import requests
-import os
 
 def audio2text(file):
-    # 获取文件路径
-    current_dir = os.getcwd()
-    file_path = os.path.join(current_dir, file)
-    print(file_path)
+    api_url = "http://localhost:8080/v1/audio/transcriptions"
+    #api_url = "http://166.111.80.169:8080/v1/chat/completions"
+    headers = {}
+    data = {
+        "model": "whisper-1"  
+    }
+    files = {
+        "file": ("audio_file", open(file, "rb"))
+    }
 
-    if file.endswith(".wav"):
-        url = "http://166.111.80.169:8080/v1/audio/transcriptions"
-        
-        # 删除显式的 "Content-Type" 头部
-        headers = {}
-        
-        # 确保参数格式正确
-        files = {
-            "file": (file, open(file_path, "rb")),  # 使用元组指定文件名和文件内容
-            "model": ("", "whisper-1")  # 正确指定模型参数
-        }
-        
-        response = requests.post(url, headers=headers, files=files)
-        if response.status_code == 200:
-            transcription = response.json()
-            print(transcription)
-            return transcription
-        else: 
-            print("API请求返回状态码:", response.status_code)
-            print("响应内容:", response.content.decode('utf-8'))  # 为调试打印响应内容
-            return None
-    else:
-        print("文件不以 .wav 结尾")
-
+    try:
+        response = requests.post(api_url, headers=headers, data=data, files=files)
+        response.raise_for_status()  # 检查响应状态
+        result = response.json()  # 解析响应的JSON数据
+        text_content = result.get("text", "")
+        #print(text_content)
+        return text_content
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        return None
+    
 if __name__ == "__main__":
     audio2text('sun-wukong.wav')
