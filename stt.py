6a733d48b119b4204825ff0ba24b1872d2f49fe1
焰色圆环 <88589612+Circular-Aflame@users.noreply.github.com>
2023-09-13 13:13:49 +0800

    gpt改

--- stt.py
+++ stt.py
@@ -2,36 +2,34 @@ import requests
 import os
 
 def audio2text(file):
-    # 获得文件路径
+    # 获取文件路径
     current_dir = os.getcwd()
     file_path = os.path.join(current_dir, file)
     print(file_path)
-    file_open = open(file_path, "rb")
-    file_key = file_open.read()
+
     if file.endswith(".wav"):
-        #url = "http://127.0.0.1:8080/v1/audio/transcriptions" 
         url = "http://166.111.80.169:8080/v1/audio/transcriptions"
-        headers = {  
-            "Content-Type": "multipart/form-data; boundary=<calculated when request is sent>",
-        }
-        """
-        data = {
-            "model": "whisper-1",
-        }
-        """
+        
+        # 删除显式的 "Content-Type" 头部
+        headers = {}
+        
+        # 确保参数格式正确
         files = {
-            "file": file_key,
-            "model": "whisper-1"
+            "file": (file, open(file_path, "rb")),  # 使用元组指定文件名和文件内容
+            "model": ("", "whisper-1")  # 正确指定模型参数
         }
+        
         response = requests.post(url, headers=headers, files=files)
         if response.status_code == 200:
             transcription = response.json()
             print(transcription)
             return transcription
         else: 
-            print("API request failed with status code:", response.status_code)
+            print("API请求返回状态码:", response.status_code)
+            print("响应内容:", response.content.decode('utf-8'))  # 为调试打印响应内容
             return None
     else:
-        print("The file does not end with .wav")
+        print("文件不以 .wav 结尾")
+
 if __name__ == "__main__":
     audio2text('sun-wukong.wav')
